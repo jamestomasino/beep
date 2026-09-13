1d9f3cac175199930da80b006cc589ad9befb807
James Tomasino <james@tomasino.org>
2026-09-13 20:17:28 +0000

    release: bump to 1.0.0 (first major)

--- src/beep.ads
+++ src/beep.ads
@@ -4,6 +4,6 @@ package Beep is
    --  Single source of truth for the version string.
    --  Keep in sync with the "version" field in alire.toml.
    --  Bump both together on a release.
-   Version : constant String := "0.1.9";
+   Version : constant String := "1.0.0";
 
 end Beep;
